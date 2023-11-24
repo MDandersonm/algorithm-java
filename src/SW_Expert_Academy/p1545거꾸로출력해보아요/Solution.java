2df881c60dc1aa8cb13a74c6b5e513840da468a7
KimYeongJIn <125858342+MDandersonm@users.noreply.github.com>
2023-11-25 02:51:55 +0900

    SW_Expert_Academy p1545 거꾸로출력해보아요

---
+++ src/SW_Expert_Academy/p1545거꾸로출력해보아요/Solution.java
@@ -0,0 +1,17 @@
+package SW_Expert_Academy.p1545거꾸로출력해보아요;
+
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+        for(int i=t; i>=0; i--){
+            System.out.print(i+" ");
+
+        }
+
+
+    }
+}
