2078f26efc6ff7b48c3c7a3d3f3b4c054e254b81
KimYeongJIn <125858342+MDandersonm@users.noreply.github.com>
2023-12-02 13:02:57 +0900

    SW_Expert_Academy p1936_1대1가위바위보

---
+++ src/SW_Expert_Academy/p1936_1대1가위바위보/Solution.java
@@ -0,0 +1,19 @@
+package SW_Expert_Academy.p1936_1대1가위바위보;
+
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        if (a>b){
+            System.out.println('A');
+        }
+        else{
+            System.out.println('B');
+        }
+
+    }
+}
