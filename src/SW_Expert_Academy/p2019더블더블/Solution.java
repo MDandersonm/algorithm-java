a9bd50cc21c2ae73acd3c7dd05c5014b1538b3cb
KimYeongJIn <125858342+MDandersonm@users.noreply.github.com>
2023-12-04 01:45:54 +0900

    SW_Expert_Academy p2019더블더블

---
+++ src/SW_Expert_Academy/p2019더블더블/Solution.java
@@ -0,0 +1,22 @@
+package SW_Expert_Academy.p2019더블더블;
+
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+        int sum=1;
+        System.out.print(sum+" ");
+        for(int i=0; i<t;i++){
+            sum*=2;
+            System.out.print(sum+" ");
+
+        }
+
+
+
+    }
+
+}
