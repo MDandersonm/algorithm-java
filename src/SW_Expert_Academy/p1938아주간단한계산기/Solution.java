ba64b12f2e59e9a2bdb5e7dacb51d1a0cc5a7e8c
KimYeongJIn <125858342+MDandersonm@users.noreply.github.com>
2023-12-03 09:22:31 +0900

    SW_Expert_Academy p1938아주간단한계산기

---
+++ src/SW_Expert_Academy/p1938아주간단한계산기/Solution.java
@@ -0,0 +1,16 @@
+package SW_Expert_Academy.p1938아주간단한계산기;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        System.out.println(a+b);
+        System.out.println(Math.abs(a-b));
+        System.out.println(a*b);
+        System.out.println(a/b);
+
+    }
+}
