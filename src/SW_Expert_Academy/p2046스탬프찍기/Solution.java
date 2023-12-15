5cad3f74d5314fc09c6b62140d023fbce5bab382
KimYeongJIn <125858342+MDandersonm@users.noreply.github.com>
2023-12-16 01:14:36 +0900

    SW_Expert_Academy p2046스탬프찍기

---
+++ src/SW_Expert_Academy/p2046스탬프찍기/Solution.java
@@ -0,0 +1,14 @@
+package SW_Expert_Academy.p2046스탬프찍기;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        System.out.println(Math.abs(a-b)+1);
+
+
+    }
+}
