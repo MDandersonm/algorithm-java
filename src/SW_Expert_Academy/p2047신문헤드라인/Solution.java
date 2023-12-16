c4201987bde935d9139fe3f80d9bf816fa7ccb16
KimYeongJIn <125858342+MDandersonm@users.noreply.github.com>
2023-12-17 02:38:06 +0900

    SW_Expert_Academy p2047신문헤드라인

---
+++ src/SW_Expert_Academy/p2047신문헤드라인/Solution.java
@@ -0,0 +1,15 @@
+package SW_Expert_Academy.p2047신문헤드라인;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String t=sc.nextLine();
+        String result=t.toUpperCase();
+        System.out.println(result);
+
+
+
+    }
+}
